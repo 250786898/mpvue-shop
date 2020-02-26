6572e747a1fba9be822119a2b6374d455facdd44
Xu shining <1015621775@qq.com>
2020-02-26 08:43:56 +0800

    优惠券点击区域问题

--- src/pages/live/index.vue
+++ src/pages/live/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <navigator
-      url="/pages/live-packages/index/main"
+      url="plugin-private://wx2b03c6e691cd7370/pages/live-player-plugin?room_id=123"
       open-type="navigate"
       hover-class="none">
       123
